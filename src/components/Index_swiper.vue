2999c3ab570cdd1dc6402dd9999713643fa4d64c
RuntuS <893119806@qq.com>
2020-06-30 11:04:13 +0800

    wanchenglunbotu

--- src/components/Index_swiper.vue
+++ src/components/Index_swiper.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="Index_swiper" v-bind:style="{width : imageWidth + 'px', 'overflow' : 'hidden',height : imageHeight*0.7 + 'px'} ">
-        <div  id="PB" v-bind:style="{width : photoBoxWidth + 'px','margin-left' :'-' + scrollWidth + 'px'}">
+        <div  id="PB" v-bind:style="{width : listImages.length * imageWidth + 'px','margin-left' :'-' + scrollWidth + 'px'}">
             <div v-for="(item,index) in listImages" class="imageBox" v-bind:style="{height : imageHeight + 'px', width: imageWidth + 'px'}"> <img v-bind:src="item.imageUrl" alt="图片"></div>
         </div>
         <div class="pointerBox">
-            <div class="pointer" v-for="(item,index) in pointerArr"></div>
+            <div class="pointer noneChoosen"  v-bind:id="'pointer' + index" v-for="(item,index) in pointerArr" v-on:click="chooseImage(index)"></div>
         </div>
     </div>
 </template>
@@ -12,30 +12,25 @@
 <script>
     export default {
         name: "Index_swiper",
+        props : ["urls","scroll_timer"],
         data(){
             return {
-                listImages : [
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/pixiv15.jpg"},
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
-                    {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/pixiv15.jpg"},
-                ],
+                listImages : this.urls,
                 imageHeight : "",
                 imageWidth : "",
                 photoBoxWidth : 0,
                 scrollWidth : 0,
                 scrollCount : 0,
                 IntervalSignal : "",
-                pointerArr : []
+                pointerArr : [],
+                timer : this.scroll_timer
             }
         },
         created() {
-
             this.imageHeight = window.innerHeight;
             this.imageWidth = window.innerWidth;
             this.photoBoxWidth = this.listImages.length * this.imageWidth;
+            // console.log(urls)
         },
         mounted() {
             var that = this;
@@ -46,9 +41,7 @@
                     that.imageWidth = window.innerWidth;
                 })()
             }
-            // setInterval(() => {
-            //
-            // },1000)
+
             $("#PB").addClass("photo");
             that.listImages.forEach((value,index) => {
                 if(index < that.listImages.length - 1)
@@ -56,23 +49,37 @@
                     console.log(index)
                     that.pointerArr.push(value);
                 }
-
             })
+            setTimeout(() => {
+                let dom = $("#pointer0");
+                dom.removeClass("noneChoosen");
+                dom.addClass("choosen");
+            },0)
+
+
             this.startScroll();
         },
         methods : {
             startScroll(){
                 var that = this;
+
+                // $("#pointer0").removeClass("noneChoosen");
+                // $("#pointer0").addClass("choosen");
                 //信号记录
                 this.IntervalSignal = setInterval(() => {
                     if (that.scrollCount < that.listImages.length - 2) //0 1 2 3
                     {
+                        // console.log(document.getElementById("pointer0"));
+                        that.leavePointer(that.scrollCount);
                         that.scrollCount = (that.scrollCount + 1) % that.listImages.length;
                         that.scrollWidth = that.imageWidth * that.scrollCount;
-                    } else if (that.scrollCount === that.listImages.length - 2) { //即将跳转到最后一张图片，最后一张和第一张图片是一样的，应该取消样式
+                        that.enterPointer(that.scrollCount);
 
+                    } else if (that.scrollCount === that.listImages.length - 2) { //即将跳转到最后一张图片，最后一张和第一张图片是一样的，应该取消样式
+                        that.leavePointer(that.scrollCount);
                         that.scrollCount++; //跳转
                         that.scrollWidth = that.imageWidth * that.scrollCount;
+                        that.enterPointer(0);
                         //注意，图片移动需要1s的时间，那么这里的延迟必须是大于1s，否则图片滑动会在中途卡断
                         setTimeout(() => {
                             $("#PB").removeClass("photo");
@@ -87,7 +94,25 @@
                         //    而实现这一个过程的方法是异步
                         }, 2000)
                     }
-                },4000)
+                },that.timer)
+            },
+            enterPointer(number){
+                var dom = $(`#pointer${number}`);
+                dom.removeClass("noneChoosen");
+                dom.addClass("choosen");
+            },
+            leavePointer(number){
+                var dom = $(`#pointer${number}`);
+                dom.removeClass("choosen");
+                dom.addClass("noneChoosen");
+            },
+            chooseImage(index){
+                clearInterval(this.IntervalSignal); //关闭计时器
+                this.leavePointer(this.scrollCount);
+                this.enterPointer(index);
+                this.scrollCount = index;
+                this.scrollWidth = this.imageWidth * this.scrollCount;
+                this.startScroll();
             }
         }
 
@@ -130,7 +155,7 @@
         display: flex;
         bottom: 5px;
         left: 50%;
-        transform:translateX(-50%);
+        transform: translateX(-50%);
         justify-content: space-around;
     }
 
@@ -139,9 +164,20 @@
     .pointer{
         width: 20px;
         height: 20px;
-        background: rgba(220,200,200,0.7);
         border-radius: 10px;
+        transition: background-color 0.5s ease-in-out;
+        cursor: pointer;
+    }
+    .pointer:hover{
+        background-color: rgba(255,255,255,0.8);
+    }
+    /*没有被选择时*/
+    .noneChoosen{
+        background: rgba(220,200,200,0.7);
+    }
 
+    .choosen{
+        background-color: rgba(255,255,255,0.8);
     }
 
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="index">
-       <Index_swiper></Index_swiper>
+       <Index_swiper v-bind:urls="urls" v-bind:scroll_timer="time"></Index_swiper>
     </div>
 </template>
 
@@ -8,7 +8,20 @@
     import Index_swiper from "../components/Index_swiper";
     export default {
         name: "index",
-        components: {Index_swiper}
+        components: {Index_swiper},
+        data(){
+            return {
+                urls :
+                        [{imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/pixiv15.jpg"},
+                        {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
+                        {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
+                        {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
+                        {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/1593260140203.png"},
+                        {imageUrl : "https://lao-lan-go.oss-cn-beijing.aliyuncs.com/highQua/pixiv15.jpg"}
+                    ],
+                time : 4000
+            }
+        }
     }
 </script>
 
